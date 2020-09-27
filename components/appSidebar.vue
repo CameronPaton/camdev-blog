7122f88b41c3d852143908ff7c19a35a92dd80c1
Camdev <dounisca@yahoo.com>
2020-09-27 14:36:18 +0100

    changing components

--- components/appSidebar.vue
+++ components/appSidebar.vue
@@ -15,8 +15,19 @@
         </div>
 
         <!-- Categories -->
-        <div>
-            <app-categories></app-categories>
+        <div class="my-5 mx-5 border border-gray-400 p-3">
+            <div>
+                <h3 class="text-lg text-center my-3">Blog Categories: </h3>
+            </div>
+
+            <div class="grid md:grid-rows-3 md:grid-cols-2 md:gap-5 lg:grid-rows-2 lg:grid-cols-3 lg:gap-5">
+                <img src="../assets/images/categories/nobg/2.png" alt="blog category" />
+                <img src="../assets/images/categories/nobg/3.png" alt="blog category" />
+                <img src="../assets/images/categories/nobg/4.png" alt="blog category" />
+                <img src="../assets/images/categories/nobg/5.png" alt="blog category" />
+                <img src="../assets/images/categories/nobg/6.png" alt="blog category" />
+                <img src="../assets/images/categories/nobg/7.png" alt="blog category" />
+            </div>
         </div>
 
         <!-- Follow Me -->
@@ -40,14 +51,12 @@
 <script>
 import appSocialMedia from "../components/socialMedia";
 import appSubscribe from "../components/appSubscribe";
-import appCategories from "../components/appCategories";
 
     export default {
             name: 'appSidebar',
             components: {
                 'app-socialmedia': appSocialMedia,
                 'app-subscribe': appSubscribe,
-                'app-categories': appCategories,
             }
     };
     
