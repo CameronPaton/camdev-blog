a0c0a94f8ad221f6713696574d87be012953464a
Camdev <dounisca@yahoo.com>
2020-10-03 12:15:58 +0100

    deploy issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,9 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
+  router: {
+    base: '/camdev-blog/'
+  },
 
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
