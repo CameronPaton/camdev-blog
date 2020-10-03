1efabaed41c206b84250e12a77ee40ddc05ad035
Camdev <dounisca@yahoo.com>
2020-10-03 14:30:09 +0100

    changes to sidebar

--- components/appsidebar.vue
+++ components/appsidebar.vue
@@ -20,16 +20,6 @@
                 <h3 class="text-lg text-center my-3">Blog Categories: </h3>
             </div>
 
-            <div class="grid md:grid-rows-3 md:grid-cols-2 md:gap-5 lg:grid-rows-2 lg:grid-cols-3 lg:gap-5">
-                <img src="../assets/images/categories/nobg/2.png" alt="blog category" />
-                <img src="../assets/images/categories/nobg/3.png" alt="blog category" />
-                <img src="../assets/images/categories/nobg/4.png" alt="blog category" />
-                <img src="../assets/images/categories/nobg/5.png" alt="blog category" />
-                <img src="../assets/images/categories/nobg/6.png" alt="blog category" />
-                <img src="../assets/images/categories/nobg/7.png" alt="blog category" />
-            </div>
-        </div>
-
         <!-- Follow Me -->
         <div>
             <app-socialmedia></app-socialmedia>
